e8d0ba67817ef8b581ff5e613b6b87cbc981154c
Ryan Kwok <rkwok2@nait.ca>
2017-10-19 14:23:11 -0600

    ICA08 Complete

--- CMPE2400ica08.sql
+++ CMPE2400ica08.sql
@@ -17,25 +17,39 @@ go
 
 --q4-- NOT Complete
 select CompanyName as 'Customer Company Name', Country from Customers 
-where CustomerID in (select CustomerID
+where CustomerID in (select TOP 8 CustomerID --Top 8 must be applied in the select statement but the ordering is for the freight
 					 from Orders
-					 where Freight in ( select Distinct Top 8 Freight 
-										from Orders 	
-										order by Freight
-									  )
-									  
+					 order by Freight desc --Note that this will elminate ties from the columns reducing it to six
 					)
 					
 
 go
 
 --q5--
-select CustomerID, OrderID, CONVERT(varchar,OrderDate,106) from orders
-where OrderID in (select OrderID
-				  from  [Order Details]
-				  where Quantity in(  Quantity
-				 )
-				 ) 
+select CustomerID, OrderID, CONVERT(varchar,OrderDate,106) as 'Order Date' from orders
+where OrderID in (select TOP 3 OrderID 
+					 from [Order Details]
+					 order by Quantity desc) 
 				
 go
+
+--q6--
+select CustomerID, OrderID, CONVERT(varchar,OrderDate,106) as 'Order Date' from orders
+where OrderID in (select TOP 3 with ties OrderID --ties includes values that are equal
+					 from [Order Details]
+					 order by Quantity desc) 
+				
+go
+
+--q7--
+select CompanyName as 'Supplier Company Name', Country from Suppliers
+where SupplierID in (select SupplierID
+					 from Products
+					 where ProductID in (select Top 1 Percent ProductID
+										 from [Order Details]
+										 order by (UnitPrice*Quantity) desc
+										 )
+					)
+go
+					 
 
